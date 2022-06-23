2e986ce2d8048181239c0cc518872b54ab69be24
Oliver Nordbjerg <hi@notbjerg.me>
2022-06-23 21:48:50 +0200

    feat: add uncovered contract

--- src/Contract.sol
+++ src/Contract.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.8.15;
 
 /// Expected:
 /// - Functions: 3
-/// - Branches: 3
+/// - Branches: 3, 6 paths
 /// - Statements: 7(?)
 /// - Lines: 7
 /// - Coverage: 100%
@@ -36,7 +36,7 @@ contract Branches {
 
 /// Expected:
 /// - Functions: 1
-/// - Branches: 2(?)
+/// - Branches: 1, 2 paths
 /// - Statements: 7
 /// - Lines: 6
 /// - Coverage: 100%
@@ -53,6 +53,25 @@ contract Loop {
                 // Should be executed 5 times
                 count -= 1;
             }
+        }
+    }
+}
+
+/// Expected:
+/// - Functions: 1
+/// - Branches: 1, 2 paths
+/// - Statements: 4
+/// - Lines: 5
+/// - Coverage: 0%
+contract Uncovered {
+    function dontCallMe(uint256 a, uint256 b) external pure {
+        uint256 c = a + b;
+        uint256 d = a - b;
+        uint256 e = a + b * 2;
+
+        // I know this should have been checked earlier don't look t11s
+        if (c < 10) {
+            revert();
         }
     }
 }
